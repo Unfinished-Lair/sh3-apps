e6dc8e3cb2b6f5f23eefc158dbf5fdd664ae9b2a
Salepate <salepate@gmail.com>
2026-04-09 02:23:34 +0200

    feat(sh3-editor): re-export public types and verify builds

--- packages/sh3-editor/src/bundle.ts
+++ packages/sh3-editor/src/bundle.ts
@@ -1,2 +1,9 @@
 export { app } from './app';
 export { shard } from './shard';
+export type {
+  EditorApi,
+  EditorDocument,
+  MatchingConfig,
+  ToolbarAction,
+  OpenDocumentOptions,
+} from './types';

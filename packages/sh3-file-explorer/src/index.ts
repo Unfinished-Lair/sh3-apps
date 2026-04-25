522031189ea599c36db8e172ad5e337835d9ff0a
Salepate <salepate@gmail.com>
2026-04-26 01:57:14 +0200

    feat(sh3-file-explorer): re-export documentBadge types

--- packages/sh3-file-explorer/src/index.ts
+++ packages/sh3-file-explorer/src/index.ts
@@ -1,3 +1,10 @@
 export { app } from './app';
 export { shard } from './shard';
-export { SELECTION_ACTION_POINT, type SelectionAction } from './contributions';
+export {
+  SELECTION_ACTION_POINT,
+  DOCUMENT_BADGE_POINT,
+  type SelectionAction,
+  type Badge,
+  type BadgeDoc,
+  type DocumentBadgeProvider,
+} from './contributions';
